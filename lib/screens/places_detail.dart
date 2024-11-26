58b5c67900500474b5851d8374a05195d3ad521c
jihanurrahman33 <jihanurrahman33@gmail.com>
2024-11-26 14:03:20 +0600

    not final

--- lib/screens/places_detail.dart
+++ lib/screens/places_detail.dart
@@ -8,7 +8,7 @@ class PlacesDetail extends StatelessWidget {
   String get locationImage {
     final lat = place.location.latitude;
     final lng = place.location.longitude;
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=AIzaSyD0Z1cfRa2w2PwveauKDIIa9BtRTldsUB4';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$lng=&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$lng&key=';
   }
 
   @override
